0fdfcd6371693e3c6f70f9efca2f4321c71c24e8
Chris Guy <christien.guy@gmail.com>
2019-05-17 14:21:47 +0100

    Fix theme context import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
-import { ToggleThemeContext } from '@themes/'
+import { ToggleThemeContext } from '@themes'
 import { ToggleThemeType, ThemeState } from '@src/ThemeProvider'
 
 const Wrapper = styled.main`
